6307e0319ffd6a38a2351f8018427bc37e83482b
Sam Magid <sam@samBook.local>
2025-03-10 18:08:07 -0400

    react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Button from "@mui/material/Button";
+
+function App() {
+
+  return (
+    <>
+      <div class="mainDiv">
+        <h1>Welcome to FontFindr.</h1>
+        <Button component="a" href="mailto:team@fontfindr.io" color="black" variant="contained">
+          Contact Us
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default App
